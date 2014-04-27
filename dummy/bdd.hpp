439c14403c133c1be25d532a8dd3e58f7a0a3ac3
Henry Kielmann <henrykielmann@gmail.com>
2014-04-27 16:40:47 +0200

    Fixed bdd

--- dummy/bdd.hpp
+++ dummy/bdd.hpp
@@ -10,26 +10,17 @@ namespace dummy
 class Describe
 {
 public:
-    Describe( const char* subject );
-    Describe& it( const char* behaviour, dummyTestFunction fn );
+    Describe( const char* subject ) : m_Subject(subject) {}
+
+    Describe& it( const char* behaviour, dummyTestFunction fn )
+    {
+        dummyAddTest((m_Subject+" "+behaviour).c_str(), fn);
+        return *this;
+    }
 
 private:
     std::string m_Subject;
 };
-
-
-// ---- implementation ----
-
-Describe::Describe( const char* subject ) :
-    m_Subject(subject)
-{
-}
-
-Describe& Describe::it( const char* behaviour, dummyTestFunction fn )
-{
-    dummyAddTest((m_Subject+" "+behaviour).c_str(), fn);
-    return *this;
-}
 
 }
 
