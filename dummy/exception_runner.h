a42b39c3e55f6eaac7bb198e713989284bf660cb
Henry Kielmann <henrykielmann@gmail.com>
2014-05-08 13:07:18 +0200

    Added config.h.in

--- dummy/exception_runner.h
+++ dummy/exception_runner.h
@@ -2,6 +2,11 @@
 #define __DUMMY_EXCEPTION_RUNNER_H__
 
 #include "core.h"
+#include "config.h"
+
+#if !defined(DUMMY_CXX_MODE)
+#warning "DUMMY_CXX_MODE is not defined, error descriptions may not be accurate."
+#endif
 
 #ifdef __cplusplus
 extern "C"
